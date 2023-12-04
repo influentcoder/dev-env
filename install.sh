66fd8229cd0d1568e6cb4b80d43894ae2fea2693
Guillaume J Humbert <guillaume.j.humbert@pm.me>
2023-12-04 19:30:31 +0800

    Config for tmux

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #/usr/bin/env zsh
 
+require() {
+    ret=0
+    for i; do
+        if ! type $i > /dev/null; then
+            echo "Missing requirement: $i"
+            ret=1
+        fi
+    done
+    return $ret
+}
+
+require git bash tmux zsh wget tar
+if [ $? -ne 0 ]; then
+    echo "Cannot proceed further, exiting"
+    exit 1
+fi
+
 # Prompt the user
 echo "WARNING: By continuing, this might override some existing configs."
 echo "Please type 'continue' to proceed or 'abort' to exit the script."
@@ -10,6 +27,12 @@ read -p "Enter your choice: " user_choice
 install() {
     SCRIPT_DIR=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
     cp ${SCRIPT_DIR}/.zshrc ${HOME}/.zshrc
+    mkdir -p ${HOME}/.config/tmux
+    cp ${SCRIPT_DIR}/.config/tmux/tmux.conf ${HOME}/.config/tmux
+    if test ! -d ~/.tmux/plugins/tpm; then
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&\
+            ~/.tmux/plugins/tpm/bin/install_plugins
+    fi
     echo "Done"
 }
 
